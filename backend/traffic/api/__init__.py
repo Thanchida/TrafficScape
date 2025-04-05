ebc866a49148ccc199adde1cf8a2cacb730008e9
Thanchida <thanchida.a@ku.th>
2025-04-05 16:37:51 +0700

    Improve API path

--- backend/traffic/api/__init__.py
+++ backend/traffic/api/__init__.py
@@ -1,7 +1,5 @@
 from .traffic import TrafficController
 from .weather import WeatherController
-from .descriptive import DescriptiveController
-from .distribution import DistributionController
 from .statistic import StatisticController
 
 __all__ = [
