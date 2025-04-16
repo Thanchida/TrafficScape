ffdd2e68f1a4e5bc2e07621c725dea614d9ee0b2
Nichapat Sirinantanakul <nichapat.sirin@ku.th>
2025-04-16 10:47:06 +0700

    Update cypress config

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,7 +1,16 @@
-export default {
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
+    supportFile: 'cypress/support/e2e.ts',
   },
-};
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+});
